bd8f8c7fe2876cbddf97cc0c965a900cdeda8a68
Sagar Bade Shrestha <itssagarshrestha@gmail.com>
2020-04-02 00:16:37 +0545

    finally

--- pass_man.py
+++ pass_man.py
@@ -1,37 +1,39 @@
 #PROGRAM TO STORE PASSWORDS
-
+import os
 import json
-accDict = {}        #used to store master name and password
+
+fp = open("master_pass.json","a+")
+if os.stat("master_pass.json").st_size ==0 :
+    dummy = {"Username" : "Password"}
+    json.dump(dummy, fp, indent=4)
+fp.close()
+
 def sign_in() :
     pass
 
-
 def create_acc() :
-    account = open("Master_pass.json","w+")
-    #json_acc = json.load(account)
 
-    
+    accDict = {}        #used to store master name and password
     #asking for uname n pass
     uName = input("Enter username :")
     mPass1 = input("Enter your password : " )
 
-    #checking if pass entered 2 times is correct
+    #checking if pass entered 2 times is correct n dumping it to json
     while True :
         mPass2 = input("Re-enter your password : ")
         if mPass1 == mPass2 :
             accDict.update({uName : mPass1})
+            with open("master_pass.json","r+") as fp :
+                data = json.load(fp)
+                data.update(accDict)
+                fp.seek(0)
+                json.dump(data, fp, indent=4)
+                print("Account Created!!")
             break
         else :
             print("Password Mismatch!")
     
-    #dumping the dictionary to text file
-    
-    print("Account created successfully!!")
     sign_in()
-
-    """for i in accDict.items() :
-        print(i)"""
-    account.close()
 
 #starting point 
 print("Do you want to :\n 1. Create a new account \n 2. Sign in with existing account ")
